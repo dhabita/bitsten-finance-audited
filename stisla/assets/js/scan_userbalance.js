6ba803354c6bd74f9f5359c0ddf11c3adcc7fcb5
kindcow <49510661+dhabita@users.noreply.github.com>
2021-08-26 17:21:10 +0700

    up

--- stisla/assets/js/scan_userbalance.js
+++ stisla/assets/js/scan_userbalance.js
@@ -1,25 +1,14 @@
-function aa(){
-    for(a=0;a<12;a++){
-     
-     WALLET.getBalanceLP(a); 
-     WALLET.getPendingReward(a); 
-     WALLET.getWalletLpBalance(a);
-     
+function aa() {
+    for (a = 0; a < 12; a++) {
+        WALLET.getBalanceWbst();
+        WALLET.getBalanceBusd();
+        WALLET.getBalanceLP(a);
+        WALLET.getPendingReward(a);
+        WALLET.getWalletLpBalance(a);
+
     }
 }
 
 
-setInterval(aa
-,10000);
+setInterval(aa, 10000);
 aa();
-
-
- 
-
-
-
-
-
-
-
-
